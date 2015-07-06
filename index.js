85b8b4e8ca4ee8bf39c6292d3eecfd941837119c
John Hatcher <jhatcher@jibe.com>
2015-07-06 09:24:00 -0400

    PENDING state is now shared between processes

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 var express = require('express');
+var redis   = require("redis");
 var app     = express();
+var client  = redis.createClient({
+  detect_buffers: true
+});
 
 // basic static assets
 app.use('/assets', express.static(__dirname + '/public'));
 app.use('/index.html', function (req, res) {
   res.sendFile(__dirname + '/public/index.html');
+});
+
+// clear redis state
+app.get('/redis/clear', function (req, res) {
+  client.del('sample', function (err) {
+    err? res.send(500, err) : res.send(200);
+  });
 });
 
 // mount the socket.io channels
